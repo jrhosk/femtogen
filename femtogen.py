d10663370fb986d92e40761ab4e1472c1f826c84
Joshua Hoskins <jhoskins@jlab.org>
2020-02-12 12:00:54 -0500

    added a place holder cross-section generator in the form of an energy loss calculation.

--- femtogen.py
+++ femtogen.py
@@ -1,9 +1,13 @@
-from scipy import constants
 import math
-from dataclasses import dataclass
 import configparser
 import os.path
+import random
+import numpy as np
+import matplotlib.pyplot as plt
 
+from scipy import constants
+from dataclasses import dataclass
+from tqdm import tqdm
 
 class FemtoGen:
     def __init__(self, *args, **kawgs):
@@ -22,9 +26,10 @@ class FemtoGen:
         """
 
         """
-        if os.path.isfile('{base}/femtogen/{config}/config.ini'.format(base=os.getcwd(), config=self.config)):
+
+        if os.path.isfile('{base}/{config}/config.ini'.format(base=os.getcwd(), config=self.config)):
             self.config_parse = configparser.ConfigParser()
-            self.config_parse.read('{base}/femtogen/{config}/config.ini'.format(base=os.getcwd(), config=self.config))
+            self.config_parse.read('{base}/{config}/config.ini'.format(base=os.getcwd(), config=self.config))
 
             self.kinematics = PyStruct(self.config_parse['event']['particle'],
                                        float(self.config_parse['event']['energy']),
@@ -35,12 +40,11 @@ class FemtoGen:
             print("Failed to find configuration file. Exiting.")
             exit(1)
 
-    def calculate_cross_section(self):
-        self.cross_section = 0.5 * constants.pi * math.pow(1, 2) * math.pow(1, 2) * math.pow(constants.alpha,
-                                                                                             2) * math.pow(
-            (0.197) / (self.kinematics.energy), 2) * (1 / math.pow(1 - math.cos(self.kinematics.theta), 2))
-        return self.cross_section
-
+    def calculate_cross_section(self, theta: 'np.array')->'iterator':
+        self.kinematics.theta = 0
+        for beta in tqdm(theta):
+            self.cross_section = math.pow(beta, -2)*math.log( (2*math.pow(constants.speed_of_light*beta, 2))/(1-math.pow(beta, 2)) - math.pow(beta, 2))
+            yield self.cross_section
 
 @dataclass
 class PyStruct:
@@ -52,6 +56,13 @@ class PyStruct:
 
 
 if __name__ == "__main__":
+    
     femto = FemtoGen()
     femto.read_config('config')
-    print(femto.calculate_cross_section())
+
+    theta = np.array([random.uniform(0.10, 0.99) for i in range(1000)])
+    cs = np.fromiter(femto.calculate_cross_section(theta), dtype=float, count = theta.size)
+
+    fig, ax = plt.subplots()
+    ax.scatter(theta, cs)
+    plt.show()

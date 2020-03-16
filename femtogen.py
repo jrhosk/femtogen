87501a8b9d0dbd1fe9e83b5f8daea70ccd7d153e
Joshua Hoskins <jhoskins@jlab.org>
2020-03-16 09:00:53 -0400

    Syncing up my local copy and the repo version. The form factor information data file should be included in this version.

--- femtogen.py
+++ femtogen.py
@@ -1,9 +1,9 @@
 import math
 import configparser
 import os.path
-import random
 import numpy as np
 import matplotlib.pyplot as plt
+import pandas as pd
 
 from scipy import constants
 from dataclasses import dataclass
@@ -14,17 +14,28 @@ class FemtoGen:
         self.config = 'config'
         self.kinematics = 0
         self.cross_section = 0
-
+        self.data_file = ''
+        
         self.ke = 8.985551e9  # N m^2 C-2 Coulomb's constant
-
+        self.metric = self.minkowski_metric(np.identity(4))
+        
         if "config" in kawgs:
             self.config = kawgs['config']
         else:
             pass
 
+    def minkowski_metric(self, metric: 'numpy array')->'numpy array':
+        """
+           Returns the 4x4 mikowski metric with diagonal of {-1, 1, 1, 1}
+        """
+        conv_matrix = np.identity(4)
+        conv_matrix[0, 0] *= -1
+        return(np.multiply(conv_matrix, metric))
+
+    
     def read_config(self, config: str):
         """
-
+        Read configuration file from standard {base}/config directory.
         """
 
         if os.path.isfile('{base}/{config}/config.ini'.format(base=os.getcwd(), config=self.config)):
@@ -41,10 +52,21 @@ class FemtoGen:
             exit(1)
 
     def calculate_cross_section(self, theta: 'np.array')->'iterator':
+        """
+           Generator for the DVCS cross section as a function of phi
+        """
         self.kinematics.theta = 0
         for beta in tqdm(theta):
             self.cross_section = math.pow(beta, -2)*math.log( (2*math.pow(constants.speed_of_light*beta, 2))/(1-math.pow(beta, 2)) - math.pow(beta, 2))
             yield self.cross_section
+
+    def contract(self, a: 'np.array', b: 'numpy array')->'float':
+        return(np.dot(a, np.dot(self.metric, b)))
+
+    def read_data_file(self, file: str):
+        self.data_file = file
+        df = pd.read_csv(file)
+        print(df.head())
 
 @dataclass
 class PyStruct:
@@ -59,10 +81,12 @@ if __name__ == "__main__":
     
     femto = FemtoGen()
     femto.read_config('config')
+    femto.read_data_file('data/cff.csv')
+    
+#    theta = np.array([np.random.random() for i in range(1000)])
+#    cs = np.fromiter(femto.calculate_cross_section(theta), dtype=float, count = theta.size)
 
-    theta = np.array([random.uniform(0.10, 0.99) for i in range(1000)])
-    cs = np.fromiter(femto.calculate_cross_section(theta), dtype=float, count = theta.size)
+#    fig, ax = plt.subplots()
+#    ax.scatter(theta, cs)
 
-    fig, ax = plt.subplots()
-    ax.scatter(theta, cs)
-    plt.show()
+#    plt.show()

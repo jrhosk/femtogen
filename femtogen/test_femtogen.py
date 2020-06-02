23f93ebf5a98a186def338b6981ac3686f3c0564
Joshua Hoskins <jhoskins@jlab.org>
2020-06-02 15:11:37 -0400

    Add requirements file for CI pipeline.

--- femtogen/test_femtogen.py
+++ femtogen/test_femtogen.py
@@ -9,14 +9,14 @@ class MyTestCase(unittest.TestCase):
     femto = FemtoGen()
 
     def test_generate_cross_section(self):
-        print('Testing Femtogen.generate_cross_section() ...')
+        print('Testing Femtogen.generate_cross_section() ....')
 
         # Check that exception is properly raised on bad cross section type
         self.assertRaises(Exception, self.femto.generate_cross_section(0, error=None, type='wrong_type'))
 
     def test_read_data_file(self):
         # Check that invalid file
-        print('Testing Femtogen.read_data_file() ...')
+        print('Testing Femtogen.read_data_file() ....')
         self.assertRaises(AssertionError, self.femto.read_data_file,'random_file_that_doesnt_exist.csv')
 
 

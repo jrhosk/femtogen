d7a0c9a611a4cfa5ac99ef431a9033f13615dbca
Joshua Hoskins <jhoskins@jlab.org>
2020-06-13 23:49:31 -0400

    Added logger decorator function to record error messages and exceptions.
    -- log/ added
    -- pylogger.py added to create simple error message/exception logger.

--- csplot.py
+++ csplot.py
@@ -5,28 +5,28 @@ import matplotlib.pyplot as plt
 from femtogen import FemtoGen
 
 if __name__ == "__main__":
-    
     femto = FemtoGen()
     femto.load_generator(generator='UU')
     kinematics = femto.generator.read_data_file('data\cff.csv')
     femto.generator.set_kinematics(array=kinematics[0])
 
-    phi = math.radians(360)*np.array([np.random.random() for i in range(1000)])
+    phi = math.radians(360) * np.array([np.random.random() for i in range(1000)])
 
     dv = np.fromiter(femto.generator.generate_cross_section(phi,
                                                             error=femto.generator.error_generator(stdev=0.0025),
                                                             type='dvcs'), dtype=float, count=phi.size)
     bh = np.fromiter(femto.generator.generate_cross_section(phi,
-                                                  error=femto.generator.error_generator(stdev=0.0025),
-                                                  type='bh'), dtype=float, count=phi.size)
+                                                            error=femto.generator.error_generator(stdev=0.0025),
+                                                            type='bh'), dtype=float, count=phi.size)
     it = np.fromiter(femto.generator.generate_cross_section(phi,
-                                                  error=femto.generator.error_generator(stdev=0.0025),
-                                                  type='int'), dtype=float, count=phi.size)
+                                                            error=femto.generator.error_generator(stdev=0.0025),
+                                                            type='int'), dtype=float, count=phi.size)
     cs = np.fromiter(femto.generator.generate_cross_section(phi,
-                                                  error=femto.generator.error_generator(stdev=0.0025),
-                                                  type='full'), dtype=float, count=phi.size)
+                                                            error=femto.generator.error_generator(stdev=0.0025),
+                                                            type='full'), dtype=float, count=phi.size)
 
-    df = femto.make_cross_section_date_frame(cs={'dvcs':(phi, dv), 'bh':(phi, bh), 'interference':(phi, it), 'total':(phi, cs)})
+    df = femto.make_cross_section_date_frame(
+        cs={'dvcs': (phi, dv), 'bh': (phi, bh), 'interference': (phi, it), 'total': (phi, cs)})
     femto.write_cross_section_csv(df)
 
     plt.scatter(phi, cs, marker='.', color='xkcd:barney purple')
